40cd4caeb7602b98db5e81ea6aa3e41115670ecd
roncevickresimir <73893906+roncevickresimir@users.noreply.github.com>
2023-01-12 11:03:24 +0100

    qf

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import IQuest from "../../interfaces/IQuest";
+import {
+  IGetQuestsPayload,
+  useLazyGetQuestByIdQuery,
+  useLazyGetQuestsQuery,
+} from "../../services/questService";
 import MainWrapper from "../components/MainWrapper";
+import LoaderAvailableLessons from "../components/skeleton-loaders/LoaderAvailableLessons";
+import QuestItem from "../quests/components/QuestItem";
 
 const Dashboard = () => {
   const { t } = useTranslation();
+
+  const [quests, setQuests] = useState<IQuest[]>([]);
+
+  const [
+    getQuests,
+    { isLoading: listLoading, isUninitialized: listUninitialized },
+  ] = useLazyGetQuestsQuery();
+  const [getQuest] = useLazyGetQuestByIdQuery();
+
+  const [getQuestsPayload, setGetQuestsPayload] = useState<IGetQuestsPayload>({
+    search: "",
+    page: 1,
+    rpp: 10,
+  });
+
+  const fetchData = async () => {
+    const getQuestsResponse = await getQuests(getQuestsPayload).unwrap();
+    setQuests(getQuestsResponse);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <MainWrapper>
@@ -14,7 +47,41 @@ const Dashboard = () => {
               </div>
             </div>
             <div className="card--lessons__body__main flex">
-              <div className="flex--primary w--100"></div>
+              <div className="flex--primary w--100">
+                <div className="w--100">{t("DASHBOARD.QUESTS")}</div>
+
+                <div className="lessons-list">
+                  {listLoading ? (
+                    <LoaderAvailableLessons />
+                  ) : quests.length > 0 ? (
+                    quests.map((lesson: IQuest) => {
+                      return (
+                        <QuestItem
+                          key={lesson.id}
+                          quest={lesson}
+                          activeQuest={""}
+                          handleActiveQuest={() => {
+                            return;
+                          }}
+                        />
+                      );
+                    })
+                  ) : (
+                    <>
+                      <div className={`lessons-list__item mt-6`}>
+                        <div className="lessons-list__item__info">
+                          <div className="type--wgt--bold">
+                            {t("QUESTS.EMPTY_QUESTS_TITLE")}
+                          </div>
+                          <div className="type--color--brand">
+                            {t("QUESTS.EMPTY_QUESTS_LIST")}
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/features/quests/components/QuestItem.tsx
+++ src/features/quests/components/QuestItem.tsx
@@ -1,6 +1,7 @@
-import { t } from "i18next";
-import { apiUrl } from "../../../app/baseService";
+import { useState, useEffect } from "react";
 import IQuest from "../../../interfaces/IQuest";
+import useCategories, { getCategoryById } from "../../../lookups/categories";
+import { useLazyGetCategoriesQuery } from "../../../services/categoryService";
 
 interface Props {
   quest: IQuest;
@@ -10,6 +11,26 @@ interface Props {
 
 const QuestItem = (props: Props) => {
   const { quest, activeQuest, handleActiveQuest } = props;
+
+  const [categoryOptions, setCategoryOptions] = useState<any>(useCategories);
+  const [getCategories] = useLazyGetCategoriesQuery();
+
+  const fetchData = async () => {
+    const getCategoriesResponse = await getCategories(null).unwrap();
+
+    let tCategories: any = [];
+    getCategoriesResponse.forEach((c: any) => {
+      tCategories.push({
+        label: c.name,
+        value: c.id,
+      });
+    });
+    setCategoryOptions(tCategories);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div
@@ -22,7 +43,22 @@ const QuestItem = (props: Props) => {
       <img className="lessons-list__item__img" src={quest.image} alt="quest" />
       <div className="lessons-list__item__info">
         <div className="type--wgt--bold">{quest.name}</div>
-        <div className="type--color--brand">{quest.categories[0]}</div>
+        <div className="type--color--brand">
+          {quest.categories.map((id: any) => {
+            console.log(
+              categoryOptions.filter((category: any) => category.value === id)
+            );
+            return (
+              <div key={id} className="tag--primary d--ib ml-2">
+                {`${
+                  categoryOptions.filter(
+                    (category: any) => category.value === id
+                  )[0]?.label
+                }`}
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div>{quest.published}</div>
     </div>
